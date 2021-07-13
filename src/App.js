0e2041f0f23ac9eda6b78511211fcc48da6110ad
Melyawati <luhlia75@gmail.com>
2021-07-13 09:50:35 +0800

    refine layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import Login from './pages/login/index';
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  import Dashboard from './pages/dashboard/index';
 import React from 'react'
 
 function App () {
     return (
+        <Router>
+        <div className="App">
         <div>
             <Login />
         </div>
+           <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+
+          <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          </Switch>
+        </div>
+
+
+
+      </Router>
+        
     )
 }
 
